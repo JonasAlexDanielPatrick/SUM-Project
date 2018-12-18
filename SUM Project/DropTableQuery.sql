b19c04d7eeca26b61a6b358eb0585bd0c9d58448
Alex Madsen <smillers_8@msn.com>
2018-12-18 23:02:29 +0100

    Kontakt viker hvis ikke man skriver æøå XD

---
+++ SUM Project/DropTableQuery.sql
@@ -0,0 +1,17 @@
+﻿DROP TABLE Kunde
+
+DROP TABLE Medarbejder 
+
+DROP TABLE Specialiseringer
+
+DROP TABLE Materialer 
+
+DROP TABLE Biler 
+
+DROP TABLE Tilbud 
+
+DROP TABLE Tilbud_materialer 
+
+DROP TABLE Tilbud_håndværkstimer
+
+DROP TABLE Tilbud_kørsel 
