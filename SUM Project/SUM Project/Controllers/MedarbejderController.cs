d81f935ec2826161228d8cf6ee573866fd7e73c5
Patrick Lundsgaard <Patrick.Lundsgaard@hotmail.com>
2018-12-19 23:37:23 +0100

    Download af medarbejder DONE // Small bugfix

--- SUM Project/SUM Project/Controllers/MedarbejderController.cs
+++ SUM Project/SUM Project/Controllers/MedarbejderController.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
